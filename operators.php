55addab5a3e5a58542802139a7acc3c11f97f97e
Sebastian <darksinkk@gmail.com>
2016-12-26 13:43:49 +0100

    Operators

---
+++ operators.php
@@ -0,0 +1,30 @@
+<?php
+//Arithmetic operators
+
+$num1 = 8;
+$num2 = 6;
+
+//Addition
+echo $num1 + $num2; //14
+//Subtraction
+echo $num1 - $num2; //2
+//Multiplication
+echo $num1 * $num2; //48
+//Division
+echo $num1 / $num2; //1.33333333333
+
+$x = 14;
+$y = 3;
+echo $x % $y; // 2
+
+// Assignment Operators
+
+$ao = 50;
+$ao += 100;
+echo $ao; // Outputs: 150
+
+// Comparison Operators
+
+// Logical Operators
+
+?>
