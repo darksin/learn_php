6240b2c3461df6909ecf41261c77d9f4431e8775
Sebastian <darksinkk@gmail.com>
2016-12-26 13:09:34 +0100

    Basic syntax

---
+++ basic_syntax.php
@@ -0,0 +1,10 @@
+<?php
+  echo "I love PHP!";
+  echo "<strong>This is a bold text.</strong>";
+  // This is a single-line comment
+  /*
+   This is a multi-line comment block
+   that spans over
+   multiple lines
+  */
+?>
